9f408dbeabbb8a3b60782b8c2e928822cfec8d7d
Atharv Vani <atharvavani110@gmail.com>
2023-08-08 00:40:54 +0530

    Typography and color set and some minor changes on hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,19 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        googlesans: ['google-sans', 'sans-serif'],
+      },
+      colors: {
+        "primary-blue": "#4285F4",
+        "primary-green": "#34A853",
+        "primary-yellow": "#F9AB00",
+        "primary-red": "#EA4335",
+        "primary-white": "#F1F3F4",
+        "primary-black": "#000000",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
